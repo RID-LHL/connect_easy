ddce70fa2108a0088266a5e916a1e136707d6f33
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-05 17:29:52 -0700

    Revert "please"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h2>RID - the best forever</h2>
-      <h3>this is good</h3>
     </div>
   );
 }
